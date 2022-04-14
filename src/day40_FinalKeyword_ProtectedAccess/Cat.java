53ab6f54db86adae8322a30ae123e28bf5772c6b
ulkucardak <ulkucardak@gmail.com>
2022-04-14 16:35:45 +0100

    Animal Task with final keyword is added

---
+++ src/day40_FinalKeyword_ProtectedAccess/Cat.java
@@ -0,0 +1,9 @@
+package day40_FinalKeyword_ProtectedAccess;
+
+public final class Cat extends Animal {
+
+    public Cat(String name, String breed, char gender, String color, String size, int age) {
+        super(name, breed, gender, color, size, age);
+    }
+
+}
